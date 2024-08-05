59e3347e2872cd9db16af62dcbf10fe72d2255b6
FurkanCsk <furkancoskun99@hotmail.com>
2024-08-05 20:38:01 +0300

    Add project files.

---
+++ Practice - Constructor/Baby.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice___Constructor
+{
+    public class Baby
+    {
+        public string _name;
+        public string _surname;
+        public DateTime _birthday;
+
+        public Baby()    // Default Constructor without parameters
+        {
+            CryingBaby();
+        }
+
+        public Baby(string Name, string SurName)  // Constructor with two parameters
+        {
+            _name = Name;
+            _surname = SurName;
+            CryingBaby();
+        }
+
+        public void CryingBaby()  // A method for display the crying message
+        {
+            Console.WriteLine("INGAAAAA!!!");
+            _birthday = DateTime.Now;
+        }
+
+    }
+}
---
+++ Practice - Constructor/Program.cs
@@ -0,0 +1,10 @@
+﻿using Practice___Constructor;
+
+Baby baby1 = new Baby(); // New object with default constructor
+
+Baby baby2 = new Baby("Furkan","Coşkun"); // New object with two parameters constructor
+
+Console.WriteLine($"Baby 1 - Name : {baby1._name}, Surname : {baby1._surname}, Birthday : {baby1._birthday}");  // Display this objects
+Console.WriteLine($"Baby 2 - Name : {baby2._name}, Surname : {baby2._surname}, Birthday : {baby2._birthday}");
+
+
